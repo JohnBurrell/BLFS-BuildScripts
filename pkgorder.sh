247e09af643cee424ca7c9277f692144258f7af7
John Burrell <John_burrell@hotmail.com>
2013-05-26 11:04:52 -0500

    updates

--- pkgorder.sh
+++ pkgorder.sh
@@ -1,57 +1,54 @@
 #!/bin/bash
 ./libraries.sh # libs with no dependencies
+./multimedia.sh # for alsa and basic sound libraries
 ./security.sh # for openssl
-./programming.sh # for python (only install the binary for OpenJDK)
-./libraries.sh # for glib
 ./networks.sh # for wget
-./security.sh # for cacerts
-./genutils.sh # for intltool
-./sysutils.sh # for unzip
+./programming.sh # for python2 (only install the binary for OpenJDK)
+./libraries.sh # for glib
+./sysutils.sh # for which, unzip
 ./pst.sh # for docbook-xml,xsl
-./server.sh # for db, sqlite, apache
-./security.sh # for nss
-./libraries.sh # for aspell
-./genutils.sh # for rarian
-./security.sh # for polkit
-./sysutils.sh # for udisks
-./programming.sh # for llvm and perl modules
-./networks.sh # for curl
-./programming.sh # for cmake
-./server.sh # for mysql - completes all specified packages
-./security.sh # for liboauth
-./genutils.sh # for  icon-naming-utils - completes all specified packages
-./sysutils.sh # for raptor, rasqal, redland, apache-ant
-./multimedia.sh # for alsa, basic libraries and gstreamer
-./x.sh # installs up to mesalib, which needs libdrm
+./genutils.sh # for pinentry, spidermonkey. Completes all specified packages
+./security.sh # for cacerts, openssh
+#
+./networks.sh # for neon and curl
+./programming.sh # for cmake, git, vala
+./server.sh # for db, sqlite, apache, bind and mysql
+./security.sh # if systemd - completes all specified packages. Leaves consolekit.
+./server.sh # for openldap. Completes all specified packages
+./sysutils.sh # reinstall systemd for gudev
+#
+# install x
+#
+./x.sh # install up to mesalib, which needs libdrm
 ./libraries.sh # for libdrm
-./x.sh # installs the rest of x
+./x.sh # install the rest of x
+#
 ./xlibs.sh # for atk, cairo
-./libraries.sh # for harfbuzz
-./xlibs.sh # for pango, gtk+-2
-./sysutils.sh # for dbus
-./security.sh # for consolekit - completes all specified packages
-./libraries.sh # telepathy-glib
-./programming.sh # for python modules
-./multimedia.sh # for gst-plugins
-./xlibs.sh # for gtk+-3 qt4
-./networks.sh # for avahi
-./pst.sh # for cups, cups-filters
-./programming.sh # for pyatspi - completes all specified packages
-./libraries.sh # for libunique, librsvg
-./networks.sh # for samba
-./xsoftware.sh # for the gimp
-./pst.sh # for gutenprint - completes all specified packages
+./libraries.sh # for harfbuzz, libgsf, poppler
+./xlibs.sh # for pango, gtk+-2, gtk+3
+#
+./multimedia.sh # for gstreamer and gst-plugins
+./sysutils.sh # for colord
+./xlibs.sh # for qt4
+./libraries.sh # for libglade, libunique and librsvg. Leaves telepathy-fstream and missn
+./programming.sh # for pygtk. Completes all specified packages
+./xsoftware.sh # for gimp and xdg-utils. Completes all specified packages
+./pst.sh # for cups. Completes all specified packages
+./networks.sh # for avahi, wpa and samba
+
 # all packages that do not depend on gnome have been installed
 # now install gnome platform
+
 ./gnomeplatform.sh
 ./networks.sh # for libsoup, networkmanager - completes all specified packages
 ./multimedia.sh # for libcanberra - completes all specified packages
 ./libraries.sh # for telepathy-mission - completes all specified packages
 ./sysutils.sh # for ibus - completes all specified packages
-./xlibs.sh # for webkitgtk
+./xlibs.sh # for webkitgtk - completes all specified packages
 ./gnomedesktop.sh
 ./gnomeplatform.sh # completes all specified packages
-./gnomedesktop.sh  # completes all specified packages
-./gnomeutils.sh # completes all specified packages
-./btscrpts.sh # install bootscripts for random, iptables, dbus, httpd, mysql, networkmanager and cups
+./gnomedesktop.sh # completes all specified packages
+./gnomeutils.sh # for gdm. Completes all packages
+# if using sysvinit, then
+./btscrpts.sh # install bootscripts for random, iptables, dbus, httpd, mysql, networkmanager, cups and bluetooth
 ./browser.sh # installs firefox
