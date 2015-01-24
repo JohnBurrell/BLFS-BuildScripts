02427e1ba6225dfe84f32c71810bcda37155ba28
John Burrell <John_burrell@hotmail.com>
2015-01-24 12:52:32 -0600

    General update

--- pkgorder.sh
+++ pkgorder.sh
@@ -16,7 +16,7 @@
 ./sysutils.sh # for which, unzip
 ./libraries.sh # for glib
 ./java.sh # only the binary for OpenJDK is installed at this stage
-./genutils.sh # for pinentry, js.
+./genutils.sh # for pinentry, tidy, dialog, qtchooser
 ./security.sh # for cacerts, openssh
 ./pst.sh # for docbook-xml,xsl
 ./programming.sh # for vala and xml-simple
@@ -26,48 +26,56 @@
 #
 ./x.sh # install up to mesalib, which needs libdrm
 ./genutils.sh # for rarian and gtk-doc - leaves imagemagick 
-./sysutils.sh # reinstall systemd with gudev
-./libraries.sh # for libgee, libgusb which needs systemd with gudev
-./xlibs.sh # for libdrm, atk, atkmm, shared-mime-info
+./sysutils.sh # reinstall systemd with gudev and dbus for dbus-launch
+./libraries.sh # for libmbim, dbus-glib and libgusb which needs systemd with gudev
+./xlibs.sh # for libdrm, atk, atkmm, shared-mime-info, gdk-pixbuf and at-spi2
 ./networks.sh # for neon, curl and netctl
 ./programming.sh # for tk, python2 and 3, cmake, git, php and subversion
-./wayland.sh # mesalib needs wayland in this build (currently disabled in the book)
+./multimedia.sh # for smpeg, libcdio, opal, libvdpau and gstreamer
+./x.sh # install up to xorg-server which needs libepoxy
+./xlibs.sh # for cairo, cairomm, glu, freeglut, libepoxy
 ./x.sh # install the rest of x
 #
 ./security.sh # for polkit, nss, cyrus-sasl, liboauth - completes specified packages
-./xlibs.sh # for cairo, cairomm
 ./libraries.sh # for libical, harfbuzz, libgsf, poppler
 ./xlibs.sh # for pango, gtk+2, gtk+3, clutter, clutter-gtk
 ./sysutils.sh # for raptor, rasqal, redland, udisks, upower and colord.
-./multimedia.sh # for gstreamer and plugins
-./libraries.sh # for libglade, librsvg
+./libraries.sh # for libglade, librsvg and reinstall freetype. Leaves openobex
+./multimedia.sh # for gstreamer plugins, gst-libav and libass
 ./programming.sh # for remaining python modules and pysol - completes specified packages
-./xsoftware.sh # for gimp, xdg-utils. Leaves icedtea-web.
+./xsoftware.sh # for gimp, xdg-utils. Leaves icedtea-web and pidgin.
 ./pst.sh # for cups - completes specified packages
 ./server.sh # for mysql and openldap - completes specified packages
-./xlibs.sh # for colord-gtk, clutter-gst, qt4 and xulrunner
-./networks.sh # for wpa_supplicant, samba
-./sysutils.sh # for bluez and obexd. Leaves ibus
+./xlibs.sh # for colord-gtk, clutter-gst and qt4
+./networks.sh # for wpa_supplicant, samba, wpa_supplicant and avahi.
+./sysutils.sh # for bluez
+./libraries.sh # for openobex - completes specified packages`
+./sysutils.sh # for obexds. Leaves ibus
+./telepathy.sh # for telepathy-glib, telepathy-logger and telepathy-idle
 #
 # most of the packages that do not depend on gnome have been installed
 # now install gnome platform
 #
-./gnomeplatform.sh
-./networks.sh # for libsoup, networkmanager, geoclue, squid - completes specified packages
-./multimedia.sh # for v4l, libcanberra, pulseaudio, ffmpeg - completes specified packages
-./libraries.sh # for telepathy-farstream and mission-control - completes specified packages
+./gnomeplatform.sh # installs up to libwnck - need libsoup next.
+./networks.sh # for glib-networking, libsoup, networkmanager, geoclue - completes specified packages
+./multimedia.sh # for v4l, libcanberra, pulseaudio, ffmpeg and vlc. Leaves grilo-plugins
+./xsoftware.sh # for pidgin
+./telepathy.sh # farstream, gabl, salut and haze - completes specified packages
 ./sysutils.sh # for ibus - completes specified packages
-./genutils.sh # for ImageMagick - completes specified packages
-./wayland.sh # for weston - completes specified packages
-./xlibs.sh # for webkitgtk2
-./java.sh # for junit and openjdk - completes specified packages
+./genutils.sh # for appstream-glib, appdata-tools and ImageMagick - completes specified packages
+./xsoftware.sh # for inkscape. Leaves icedtea-web.
+./xlibs.sh # for webkitgtk2 and notification-daemon - completes specified packages
+./java.sh # for openjdk - completes specified packages
 #
-./gnomedesktop.sh
 ./gnomeplatform.sh # completes specified packages
-./gnomedesktop.sh # completes specified packages
-./gnomeutils.sh # for gdm - completes specified packages
+./gnomeapps.sh # for baobab, gedit, gnome-calculator, up to gnome-chess
+./gnomedesktop.sh # for caribou, up to yelp
+./gnomeapps.sh # for brasero to evolution.
+./gnomedesktop.sh # for bluetooth, control-center and gnome-shell - completes specified packages
+./gnomeapps.sh # for terminal and user-share - completes specified packages
+./multimedia.sh # for grilo-plugins -  - completes specified packages
 # if using sysvinit, then
 ./btscrpts.sh # install bootscripts for random, iptables, dbus, httpd, mysql, networkmanager, cups and bluetooth
 # if using systemd, need to activate these using systemctl enable <service name>
-./browser.sh # installs firefox or seamonkey
+./browser.sh # installs firefox or seamonkey. Choose chromium if depot_tools and the src dir are in place.
 ./xsoftware.sh # for icedtea-web - completes all specified packages
