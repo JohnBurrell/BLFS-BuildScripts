fad5189a123508f97b6b1649b7261ca0c7ac5422
John Burrell <John_burrell@hotmail.com>
2015-09-03 10:25:57 -0500

    Updates to include a DESTDIR install

--- pkgorder.sh
+++ pkgorder.sh
@@ -7,7 +7,8 @@
 # Once x libs are installed, then install tk and then reinstall python 2 and 3.
 # ** NOTE ** do not move the position of tk - it has to come before python2 in order
 # that python2 and 3 are reinstalled after tk is installed.
-# python modules can then be installed 
+# python modules can then be installed
+#
 ./libraries.sh # libs with no dependencies
 ./multimedia.sh # for alsa and basic sound libraries
 ./security.sh # for openssl and sudo
@@ -16,66 +17,65 @@
 ./sysutils.sh # for which, unzip
 ./libraries.sh # for glib
 ./java.sh # only the binary for OpenJDK is installed at this stage
-./genutils.sh # for pinentry, tidy, dialog, qtchooser
+./genutils.sh # for pinentry, tidy, iso-codes, dialog, shared-mime-info
 ./security.sh # for cacerts, openssh
 ./pst.sh # for docbook-xml,xsl
 ./programming.sh # for vala and xml-simple
 ./server.sh # for db, sqlite, apache and bind
+./security.sh # for polkit, nss, cyrus-sasl
+./server.sh # for openldap - leaves mariadb
+./security.sh # for gnupg, krb5 and gpgme. Leaves liboauth or consolekit if using init.d
+./networks.sh # for neon, curl and reinstall wget for gnutls support
 #
 # install x
 #
 ./x.sh # install up to mesalib, which needs libdrm
-./genutils.sh # for rarian and gtk-doc - leaves imagemagick 
-./sysutils.sh # reinstall systemd with gudev and dbus for dbus-launch
+./genutils.sh # for rarian, gtk-doc, tdb - leaves imagemagick and ibus
+./sysutils.sh # reinstall systemd with gudev, dbus for dbus-launch and udisks
 ./libraries.sh # for libmbim, dbus-glib and libgusb which needs systemd with gudev
-./xlibs.sh # for libdrm, atk, atkmm, shared-mime-info, gdk-pixbuf and at-spi2
-./networks.sh # for neon, curl and netctl
+./networks.sh # for netctl and links
 ./programming.sh # for tk, python2 and 3, cmake, git, php and subversion
-./multimedia.sh # for smpeg, libcdio, opal, libvdpau and gstreamer
+./xlibs.sh # for libdrm, atk, atkmm, gdk-pixbuf and at-spi2
+./multimedia.sh # for speex, smpeg, libcdio, libvdpau and gstreamer
 ./x.sh # install up to xorg-server which needs libepoxy
 ./xlibs.sh # for cairo, cairomm, glu, freeglut, libepoxy
 ./x.sh # install the rest of x
 #
-./security.sh # for polkit, nss, cyrus-sasl, liboauth - completes specified packages
-./libraries.sh # for libical, harfbuzz, libgsf, poppler
+./security.sh # for liboauth or consolekit if using init.d - completes specified packages
+./libraries.sh # for libical, harfbuzz, libgsf, poppler and jasper
 ./xlibs.sh # for pango, gtk+2, gtk+3, clutter, clutter-gtk
 ./sysutils.sh # for raptor, rasqal, redland, udisks, upower and colord.
-./libraries.sh # for libglade, librsvg and reinstall freetype. Leaves openobex
-./multimedia.sh # for gstreamer plugins, gst-libav and libass
+./libraries.sh # for libglade, librsvg and reinstall freetype. Leaves appstream-glib and openobex
+./multimedia.sh # for gstreamer plugins, gst-libav, libass and libcanberra
 ./programming.sh # for remaining python modules and pysol - completes specified packages
 ./xsoftware.sh # for gimp, xdg-utils. Leaves icedtea-web and pidgin.
 ./pst.sh # for cups - completes specified packages
-./server.sh # for mysql and openldap - completes specified packages
-./xlibs.sh # for colord-gtk, clutter-gst and qt4
-./networks.sh # for wpa_supplicant, samba, wpa_supplicant and avahi.
-./sysutils.sh # for bluez
-./libraries.sh # for openobex - completes specified packages`
-./sysutils.sh # for obexds. Leaves ibus
+./server.sh # for mariadb - completes specified packages
+./sysutils.sh # for bluez - completes specified packages
+./libraries.sh # for openobex. Leaves appstream-glib
+./networks.sh # for wpa_supplicant, samba and avahi.
+# for gnome
 ./telepathy.sh # for telepathy-glib, telepathy-logger and telepathy-idle
-#
-# most of the packages that do not depend on gnome have been installed
-# now install gnome platform
+# for xfce
+./xfcegnome.sh # if using xfce, gnome packages for xfce
 #
 ./gnomeplatform.sh # installs up to libwnck - need libsoup next.
-./networks.sh # for glib-networking, libsoup, networkmanager, geoclue - completes specified packages
-./multimedia.sh # for v4l, libcanberra, pulseaudio, ffmpeg and vlc. Leaves grilo-plugins
-./xsoftware.sh # for pidgin
-./telepathy.sh # farstream, gabl, salut and haze - completes specified packages
-./sysutils.sh # for ibus - completes specified packages
-./genutils.sh # for appstream-glib, appdata-tools and ImageMagick - completes specified packages
-./xsoftware.sh # for inkscape. Leaves icedtea-web.
-./xlibs.sh # for webkitgtk2 and notification-daemon - completes specified packages
+./networks.sh # for glib-networking, libsoup, geoclue2 and uhttpmock
+./xlibs.sh # for colord-gtk, clutter-gst, qt4 and webkitgtk - completes specified packages
+./libraries.sh # for appstream-glib - completes specified packages
+./multimedia.sh # for v4l, libcanberra, pulseaudio, ffmpeg and vlc - leaves grilo-plugins if systemd
+./genutils.sh # for ImageMagick and ibus - completes specified packages
 ./java.sh # for openjdk - completes specified packages
+./browser.sh # for firefox
+./xsoftware.sh # for thunderbird and pidgin
+./telepathy.sh # if installing gnome, farstream, gabl, salut and haze - completes specified packages
+./xsoftware.sh # for inkscape. Leaves icedtea-web.
+./xlibs.sh # for webkitgtk and notification-daemon - completes specified packages
+# for xfce
+./xfce.sh # if instaling xfce instead of gnome
 #
-./gnomeplatform.sh # completes specified packages
-./gnomeapps.sh # for baobab, gedit, gnome-calculator, up to gnome-chess
-./gnomedesktop.sh # for caribou, up to yelp
-./gnomeapps.sh # for brasero to evolution.
-./gnomedesktop.sh # for bluetooth, control-center and gnome-shell - completes specified packages
-./gnomeapps.sh # for terminal and user-share - completes specified packages
-./multimedia.sh # for grilo-plugins -  - completes specified packages
-# if using sysvinit, then
-./btscrpts.sh # install bootscripts for random, iptables, dbus, httpd, mysql, networkmanager, cups and bluetooth
+./gnomeplatform.sh # for totem-pl-parser to folks - completes specified packages
+./gnomedesktop.sh # for caribou to gnome-shell - completes specified packages
+./gnomeapps.sh # for baobab to gnome-user-share - completes specified packages
+./btscrpts.sh # if using sysvinit - install bootscripts for random, iptables, dbus, httpd, mysql, networkmanager, cups and bluetooth
 # if using systemd, need to activate these using systemctl enable <service name>
-./browser.sh # installs firefox or seamonkey. Choose chromium if depot_tools and the src dir are in place.
-./xsoftware.sh # for icedtea-web - completes all specified packages
