78cb8890f1ec36af2b1b78291edba9e131d122fb
John Burrell <John_burrell@hotmail.com>
2021-03-26 13:54:33 +0000

    Add files via upload

--- pkgorder.sh
+++ pkgorder.sh
@@ -3,83 +3,86 @@
 # The rationale is to get x installed reasonably early, but
 # other considerations are that python needs tk to use IDLE
 # tk needs x libraries and xcb-proto needs python installed.
-# the solution here is to install python2 and 3 early but not to install any python modules
-# until tk is installed.
-# Once x libraries are installed, then install tk and then reinstall python 2 and 3.
+# the solution here is to install python2 early (python3 is now in LFS)
+# Once x libraries are installed, then install tk and then reinstall python2 and 3 for IDLE.
 # ** NOTE ** do not move the position of tk - it has to come before python2 in order
 # that python2 and 3 are reinstalled after tk is installed.
-# python modules can then be installed
+# 
 # wget is reinstalled after gnutls is installed.
-./libraries.sh # libs with no dependencies - installs to libxml2
-./multimedia.sh # alsa and basic sound libraries - installs to libcddb. jack is next - needs db
+./libraries.sh # libs with no dependencies - installs to libuv
+./multimedia.sh # alsa and basic sound libraries - installs to mpg123
 ./server.sh # db, sqlite, and apache. Next is openldap which needs cyrus-sasl
-./security.sh # iptables, nettle, cracklib, linux-pam, sudo, nss ... p11-kit, libpwquality
-./networks.sh # dhcpcd, net-tools, wget, libndp, libnl, wireless-tools, traceroute ... curl, nghttp2
-./security.sh # cacerts, openssh, p11-kit, gnutls, liboauth
-./networks.sh # wget with gnutls support, iw and neon
+./security.sh # nettle, cracklib, linux-pam, shadow, libcap, fuse3, sudo, lvm2, parted, gptfdisk, mdadm, dosfstools, libpwquality, nss, cyrus-sasl, p11-kit
+./networks.sh # dhcpcd, net-tools, wget, libndp, libnl, libmnl, libnftnl, wireless-tools, traceroute, whois, libtirpc, rpcsvc-proto, libevent, bridge-utils, libpcap, nmap, rsync, libev, curl, nghttp2, lynx
+./security.sh # nftables, ca-certs, openssh, gnutls, liboauth
+./networks.sh # wget with gnutls support, iw, neon
 ./security.sh # pkcs11-helper and nss-mdns
-./programming.sh # archive-zip, nasm, uri, slang, python2, tcl, perl modules, gdb, git, ... php
-./sysutils.sh # which, cpio, sg3_utils, unzip, zip, pciutils, usbutils, pm-utils ... rasqal, redland
-./libraries.sh # libsass, libssh2, boost, exempi, freetype, fontconfig, talloc, wayland, ... newt 
-./java.sh # Oracle binary (12.0.1)
-./genutils.sh # pinentry, dialog, rpmextract, iso-codes, bogofilter ... graphviz. Reinstall grep with pcre support
+./perlmodules.sh # module-build, test-mockmodule, super, archive-zip, uri, tie-ixhash, xml-sax-base, xml-namespace, xml-sax, xml-sax-expat, extutils-dpnds, extutls-pkgcnfg, parse-yapp, ipc-sys-simple, file-which, file-basedir, alien-build, path-tiny, file-chdir, capture-tiny, alien-libxml2, xml-libxml, xml-simple - completes specified packages
+./programming.sh # nasm, slang, python2, scons gc ruby guile  gdb, git, php, resinstall gcc for fortran support
+./sysutils.sh # which, cpio, sg3_utils, unzip, zip, minizip, pciutils, usbutils, pm-utils, acpid, gpm, raptor, rasqal, redland, bubblewrap
+./pst.sh # sgml-common, docbook-xml, docbook-xsl, itstool, opensp
+./libraries.sh # hyphen, ijs, libunwind, libsass, libssh2, boost, exempi, libedit, freetype, talloc, wayland, wayland-proto, libarchive, fontconfig, aspell, newt, libxslt, glib, gobject-intro, json-glib, glibmm, enchant, libgudev
+./java.sh # Oracle binary (15.0.2), apache-ant
+./genutils.sh # pinentry, dialog, grep, iso-codes, bogofilter, bash-completion, re2, graphviz, rpmextract, highlight, asciidoc, rarian, gtk-doc, tdb, desktop-file-utils. Reinstall grep with pcre support
+./pst.sh # xmlto
 ./networks.sh # libpsl, serf
-./pst.sh # docbook-xml, docbook-xsl, itstool, sgml-common and opensp
-./libraries.sh # libxslt, glib, libcroco, gobject-intro, gmime3, json-glib, glibmm ... mypaint-brushes
-./programming.sh # cmake, doxygen, llvm, subversion, rustc, cbindgen and vala
-./libraries.sh # yaml, exiv2, libjpeg-turbo, libmng, qpdf, gegl, rapidjson, libtiff, ... openjpeg2
+./programming.sh # cmake, doxygen, cmocka, ccache, sassc, llvm, subversion, rustc, cbindgen, vala
+./libraries.sh # giflib, json-c, libmypaint, mypaint-brushes, yaml, exiv2, libjpeg-turbo, libmng, qpdf, rapidjson, libtiff, snappy, libwebp, yajl, openjpeg2, libical, gcab, graphite2
 ./server.sh # openldap and mariadb - completes specified packages
 ./icons.sh # hicolor-icon-theme, icon-naming-utils and adwaita-icon-theme
-./security.sh # gnupg, krb5, gpgme and volume_key. Leaves polkit
-./pythonmodules.sh # cssselect to sklearn 
+./security.sh # cryptsetup, gnupg, krb5, gpgme and volume_key. Leaves polkit
+./pythonmodbin.sh # install binaries for py2-ordered-set, py2-pyparsing, py2-setuptools
+./pythonmodules.sh # py2-cssselect to pybind11
 #
 # install x
 #
-./x.sh # install up to libva, which needs libdrm
-./genutils.sh # rarian, gtk-doc, tdb and tidy - leaves imagemagick and ibus
-./sysutils.sh # systemd with gudev, dbus, bluez. udisks is next
-./libraries.sh # js52, js60, libmbim, libqmi, libbytesize, libblockdev, dbus-glib, jasper and libwacom
+./x.sh # util-macros, xbitmaps, xorg-proto, libxau, libxdmcp, xcb-proto, libevdev, libinput, libxcb, x7lib, xcb-util, xcb-util-image, xcb-utl-keysyms, xcb-utl-rndrutl, xcb-util-wm, xcb-util-cursor, xclock, libvdpau
+./genutils.sh # shared-mime-info, tidy, brotli and woff2 - leaves imagemagick and ibus
+./sysutils.sh # systemd with gudev, dbus, bluez
+./networks.sh # c-ares, libproxy, netctl, iproute2, openvpn, wpa_supplicant, bind-tools, squid
+./programming.sh # tk and reinstall python2 and 3 for idle support. nodejs, waf, valgrind
+./libraries.sh # libmbim, libqmi, libwacom, libgusb, js78, dbus-glib, libbytesize, libblockdev, jasper
 ./security.sh # polkit - completes specified packages
-./networks.sh # reinstall wpa_supplicant. Install c-ares, squid, netctl, links, libproxy and openvpn
-./programming.sh # tk and reinstall python2 and 3 for idle support, valgrind, nodejs and vala
-./pythonmodules.sh # py-xdg, py-dbus
-./xlibs.sh # libdrm, atk, atkmm, gdk-pixbuf, at-spi2 and imlib2
-./multimedia.sh # jack, libcdio, speex, gstreamer, libvpx, x264, x265, pulseaudio, espeak, speechd, smpeg
-./x.sh # install up to xorg-server which needs libepoxy
+./pythonmodules.sh # py3-numpy, py3-scipy, py3-xdg, py3-dbus, py3-dbusmock
+./xlibs.sh # libdrm, atk, atkmm, gdk-pixbuf, startup-notify, libxklavier, at-spi2, imlib2, graphene
+./multimedia.sh # celt, libcdio, speex, vamp-plugin-sdk, portsmf, serd, sord, gstreamer, libvpx, x264, wavpack, libmusicbrainz, portaudio, libgphoto2, jack2, taglib, media-player-info, x265, pulseaudio, espeak, speechd, smpeg, pysolfc
+./x.sh # libva, mesa, x7app, xcursor-themes, x7font, xkeyboard-config, xterm, xinit, dejavu-fonts
 ./xlibs.sh # cairo, cairomm, glu, freeglut, libepoxy
-./x.sh # install the rest of x
+./x.sh # xorg-server, xorg-evdev, xorg-libinput, xorg-amdgpu, twm
 #
-./libraries.sh # gcab, libgusb, harfbuzz, libgsf, libxkbcommon and poppler
-./xlibs.sh # pango, cogl, gtk+2, gtk+3, libnotify, gtk-vnc, clutter and clutter-gtk
-./libraries.sh # librsvg. Also reinstall freetype for harfbuzz support
-./sysutils.sh # udisks, upower, colord, modemmanager and weston - completes specified packages
-./multimedia.sh # gstreamer plugins, gst-libav, libass, libcanberra and ffmpeg
-./pythonmodules.sh # py-cairo, py-gobject, py-matplotlib, pyatspi and pygtk. Leaves py3-pyqt5
-./programming.sh # perl-glib and pysol - leaves codelite and cuda
-./xsoftware.sh #  xdg-utils 
-./pst.sh # xmlto and mupdf
-./gnomeplatform.sh # gnome-common up to libpeas (Note, gimp now (2018) needs gexiv2)
-./networks.sh # libnice, samba, avahi, glib-networking, libsoup, uhttpmock, geoclue2 and networkmanager 
-./xsoftware.sh # gimp and thunderbird
-./pst.sh # cups, ghostscript, mudpdf and cups-filters
-./libraries.sh # openobex. Leaves appstream-glib
+./libraries.sh # harfbuzz, libgsf, libxkbcommon and poppler
+./xlibs.sh # pango, pangomm, cogl, gtk+2, gtk+3, libnotify, gtk-vnc, clutter clutter-gtk, libdazzle, tepl
+./libraries.sh # libgxps, gspell, librsvg and babl. Also reinstall freetype for harfbuzz support
+./sysutils.sh # accountsservice, colord, modemmanager, udisks, upower
+./multimedia.sh # lv2, sratom, lilv, sdl2, gstreamer plugins, libass, libcanberra, ffmpeg, gst-libav, pavucontrol
+./pythonmodules.sh # py3-cairo, py3-gobject, py3-matplotlib, py3-atspi. py2 modules no longer installed
+./programming.sh # sysprof - leaves codelite and cuda
+./pst.sh # mupdf
+./xsoftware.sh #  xdg-utils
+./pst.sh # cups, ghostscript
+./gnomeplatform.sh # gnome-common, gset-dsktp-schms, libsecret, yelp-xsl, telp-tools, gnome-menus, libgee, libgtop, gexiv2, gjs, dconf, vte, gconf, gcr, gnome-autoar, gnome-desktop, libwnck, libpeas
+./networks.sh # glib-networking, libsoup, uhttpmock, samba, networkmanager, libnma, network-manager-applet
+./libraries.sh # gegl, libgrss, and appstream-glib - completes specified packages
+./xsoftware.sh # gimp 
 ./icons.sh # gnome-icon-theme, extras, symbolic and gnome-themes-extra - completes specified packages
 # for gnome and openelec
-./telepathy.sh # telepathy-glib, telepathy-logger
+./telepathy.sh # telepathy-glib, telepathy-logger, mission-control - completes specified packages
 # for xfce
 ./xfcegnomeplatform.sh # if using xfce, gnomeplatform packages for xfce
 #
 # if installing kodi then
-./openelec.sh # libdcadec, glew, setconf, tinyxml, p8-platform, libmicrohttpd and libcrossguid
+./openelec.sh # p8-platform, glew, setconf, tinyxml, libmodplug, libcec, libaacs, libmicrohttpd
 #
-./libraries.sh # appstream-glib - completes specified packages
 ./java.sh # openjdk
-./multimedia.sh # grilo, gst-plugins-good and farstream.
+./multimedia.sh # gst-plugins-good
 ./genutils.sh # ImageMagick and ibus - completes specified packages
-# for gnome and openelec
-./telepathy.sh # mission-control - completes specified packages
+./gnomeplatform.sh # gssdp, gupnp, gupnp-igd, totem-pl-parser, geocode-glib, grilo, grilo-plugin, libchamplain, libgweather, librest, tracker, tracker-miners, tracker3,  tracker3-miners
 #
-./xlibs.sh # clutter-gst, qt5, webkit2gtk and wxtgk
+./networks.sh # libnice
+./xlibs.sh # colord-gtk, clutter-gst, qt5-base, qt5-svg, qt5-x11extras
+./networks.sh # avahi, geoclue - completes specified packages 
+./xlibs.sh # webkit2gtk and wxtgk - completes specified packages
+./gnomeplatform.sh # goa, libgdata, evol-data-server, folks - completes specified packages
 #
 # for xfce
 ./xfcegnomedesktop.sh # gnomedesktop packages for xfce
@@ -89,17 +92,14 @@
 ./windowmanager.sh # fluxbox, openbox, i3-wm and menumaker - completes specified packages
 #
 # for gnome and openelec
-./gnomeplatform.sh # totem-pl-parser to tracker - completes specified packages
-./multimedia.sh # v4l-utils, vlc and grilo-plugins - completes specified packages
-./server.sh # pipewire - completes specified packages
-./gnomedesktop.sh # caribou to gnome-shell. Leaves gnome-control-center
+./multimedia.sh # v4l-utils, pipewire, farstream, vlc - completes specified packages
+./gnomedesktop.sh # gnome-backgrounds, gnome-video-effects, gvfs, nautilus, zenity, gnome-keyring, gnome-settings-daemon, mutter, gnome-shell-extensions, gnome-session, gdm, gnome-user-docs, catarell-fonts, yelp, gsound, gnome-bluetooth, gnome-shell
 # for gnome
-./gnomeapps.sh # baobab to seahorse - completes specified packages
+./gnomeapps.sh # baobab, cheese, gnome-calculator, gnome-color-manager, gnome-dictionary, gnome-nettool, gnome-power-manager, gucharmap, aisleriot, xboard, gnuchess, gnome-chess, gnome-disk-utility, brasero, eog, gedit, evince, gnome-clocks, gnome-system-monitor, gnome-terminal, gnome-user-share, seahorse, gnome-screenshot, gnome-tweaks - completes specified packages
 ./gnomedesktop.sh # gnome-control-center -completes specified packages
 #
-./multimedia.sh # v4l-utils, vlc and grilo-plugins - completes specified packages
-./pythonmodules.sh # py3-pyqt5 - completes specified packages
-./pst.sh # gutenprint, sane and xsane - completes specified packages
+./pst.sh # cups-filters, gutenprint, sane and xsane - completes specified packages
+./sysutils.sh # notification-daemon, weston
 #
 # ******************************************************************
 # *** install the following on the target machine, not in chroot ***
@@ -108,13 +108,14 @@
 # for openelec
 ./openelec.sh # kodi and the encoders - completes specified packages
 #
-./programming.sh # rustc, codelite and cuda - completes specified packages
+./programming.sh # codelite and cuda - completes specified packages
 ./browser.sh # firefox and chromium
 ./xsoftware.sh # inkscape and icedtea-web - completes specified packages 
 ./nvidia.sh # nvidia modules - needed if using CUDA
 ./java.sh # apache-maven and junit
 #
 # if using sysv
-./btscrpts.sh # install bootscripts for random, iptables, dbus, httpd, mysql, networkmanager, cups and bluetooth
+./btscrpts.sh # install bootscripts for random, dbus, httpd, mysql, networkmanager, cups and bluetooth
 # if using systemd, need to do these:
-# systemctl enable acpid, avahi-daemon, bluetooth, iptables, ModemManager, NetworkManager, org.cups.cupsd, squid, udisks2
+systemctl enable acpid.service avahi-daemon.service bluetooth.service nftables.service ModemManager.service NetworkManager.service org.cups.cupsd.service udisks2.service
+# note - wpa_supplicant.service and dhcpcd.service will start at boot time
