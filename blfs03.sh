fb1080be4944141c60697da2ab0e797c7d7bd85b
John Burrell <John_burrell@hotmail.com>
2021-03-26 13:52:57 +0000

    Add files via upload

--- blfs03.sh
+++ blfs03.sh
@@ -130,12 +130,8 @@ fi
 export INPUTRC
 EOF
 cat > /etc/profile.d/umask.sh << "EOF"
-# By default we want the umask to get set.
-if [ "$(id -gn)" = "$(id -un)" -a $EUID -gt 99 ] ; then
-  umask 002
-else
-  umask 022
-fi
+umask 022
+if [ "$(id -gn)" = "$(id -un)" -a $EUID -gt 99 ]; then umask 002; fi
 EOF
 cat > /etc/profile.d/i18n.sh << "EOF"
 # Set up i18n variables
